e733d130e85745980b8a280be5a28aad5462a520
kanch21 <chowdharikanchan@gmail.com>
2021-09-07 20:57:18 -0400

    version 1

---
+++ HelloKC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloKC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Guys! Hope you are doing good -KC");
+        }
+    }
+}
